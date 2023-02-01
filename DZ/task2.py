50cd20a41ed633de0f456ec00b8142ef78c21a75
brodilin ivan <brodilinivan@yandex.ru>
2023-02-01 15:20:14 +0400

    2 задача исправлена

--- DZ/task2.py
+++ DZ/task2.py
@@ -9,18 +9,15 @@ email, телефон.
 email: jackie@gmail.com, телефон: 01005321456
 """
 
-first_name = input('Введите имя: ')
-second_name = input('Введите фамилию: ')
-year_of_birth = int(input('Введите год рождения: '))
-place_to_live = input('Введите город проживания: ')
-e_mail = input('Введите e-mail: ')
-phone = input('Введите телефон: ')
-
 
 def func_output(param1, param2, param3, param4, param5, param6):
-    print(
-        f'{param1} {param2} {param3} года рождения, проживает в городе '
-        f'{param4},\n email: {param5}, телефон: {param6}')
+    return (f'{param1} {param2} {param3} года рождения, проживает в городе '
+            f'{param4},\n email: {param5}, телефон: {param6}')
 
-func_output(first_name, second_name, year_of_birth, place_to_live, e_mail,
-            phone)
+
+print(func_output(param1=input('Введите имя: '),
+                  param2=input('Введите фамилию: '),
+                  param3=int(input('Введите год рождения: ')),
+                  param4=input('Введите город проживания: '),
+                  param5=input('Введите e-mail: '),
+                  param6=input('Введите телефон: ')))
